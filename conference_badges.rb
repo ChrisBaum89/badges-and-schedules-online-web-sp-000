415a8e8ebe3e68d523b4a4719fe75c622ebcdbbb
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 00:29:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,14 +6,15 @@ end
 def batch_badge_creator(name)
   name_array = Array.new
   name.each do |i|
-    #puts "#{badge_maker(i)}"
     name_array << badge_maker(i)
   end
   name_array
 end
 
-def assign_rooms
-
+def assign_rooms(attendees)
+  new_array = Array.new
+  
+  "Hello, Edsger! You'll be assigned to room 1!"
 end
 
 def printer
